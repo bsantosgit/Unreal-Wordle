0c7a582e66bfee5cadea41421955a345a93e7029
Kushagra <kushagra.mckv@gmail.com>
2022-11-18 16:36:49 +0530

    KP
    - Created Basic MainMenuWidget Structure
    - Created a Blueprint Main Menu Design
    - Called Prototype Main Menu from Game Mode

--- Source/UnrealWordle/Private/HUD/MainMenuWidget.cpp
+++ Source/UnrealWordle/Private/HUD/MainMenuWidget.cpp
@@ -3,3 +3,76 @@
 
 #include "HUD/MainMenuWidget.h"
 
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+
+void UMainMenuWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	WordLength = 4;
+	GuessCount = 4;
+
+	if(DownWordLengthButton)
+		DownWordLengthButton->OnClicked.AddDynamic(this, &UMainMenuWidget::DownWordLengthButtonClicked);
+	if(UpWordLengthButton)
+		UpWordLengthButton->OnClicked.AddDynamic(this, &UMainMenuWidget::UpWordLengthButtonClicked);
+	if(DownGuessCountButton)
+		DownGuessCountButton->OnClicked.AddDynamic(this, &UMainMenuWidget::DownGuessCountButtonClicked);
+	if(UpGuessCountButton)
+		UpGuessCountButton->OnClicked.AddDynamic(this, &UMainMenuWidget::UpGuessCountButtonClicked);
+
+	if(WordLengthText)
+	{
+		WordLengthText->TextDelegate.BindUFunction(this, "SetWordLengthText");
+		WordLengthText->SynchronizeProperties();
+	}
+	if(GuessCountText)
+	{
+		GuessCountText->TextDelegate.BindUFunction(this, "SetGuessCountText");
+		GuessCountText->SynchronizeProperties();
+	}
+}
+
+void UMainMenuWidget::DownWordLengthButtonClicked()
+{
+	if(WordLength > 3)
+	{
+		WordLength--;
+	}
+}
+
+void UMainMenuWidget::UpWordLengthButtonClicked()
+{
+	if(WordLength < 10)
+	{
+		WordLength++;
+	}
+}
+
+void UMainMenuWidget::DownGuessCountButtonClicked()
+{
+	if(GuessCount > 3)
+	{
+		GuessCount--;
+	}
+}
+
+void UMainMenuWidget::UpGuessCountButtonClicked()
+{
+	if(GuessCount < 10)
+	{
+		GuessCount++;
+	}
+}
+
+FText UMainMenuWidget::SetWordLengthText() const
+{
+	return FText::FromString(FString::Printf(TEXT("%d"), WordLength));
+}
+
+FText UMainMenuWidget::SetGuessCountText() const
+{
+	return FText::FromString(FString::Printf(TEXT("%d"), GuessCount));
+}
--- Source/UnrealWordle/Public/HUD/MainMenuWidget.h
+++ Source/UnrealWordle/Public/HUD/MainMenuWidget.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "MainMenuWidget.generated.h"
 
+class UTextBlock;
+class UButton;
 /**
  * 
  */
@@ -13,5 +15,40 @@ UCLASS()
 class UNREALWORDLE_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UTextBlock* WordLengthText;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UTextBlock* GuessCountText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UButton* DownWordLengthButton;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UButton* UpWordLengthButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UButton* DownGuessCountButton;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="MainMenuWidget", meta=(BindWidget))
+	UButton* UpGuessCountButton;
+
+	virtual void NativeOnInitialized() override;
+
+private:
+	int32 WordLength;
+	int32 GuessCount;
+
+	UFUNCTION()
+	void DownWordLengthButtonClicked();
+	UFUNCTION()
+	void UpWordLengthButtonClicked();
+	UFUNCTION()
+	void DownGuessCountButtonClicked();
+	UFUNCTION()
+	void UpGuessCountButtonClicked();
+
+public:
+	UFUNCTION()
+	FText SetWordLengthText() const;
+	UFUNCTION()
+	FText SetGuessCountText() const;
 };
